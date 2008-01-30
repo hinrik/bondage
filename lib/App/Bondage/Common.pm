282b6ca7c99c157ee0a15a950fc7bb729db67657
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2008-01-30 05:08:37 +0000

    Fix version silliness

--- lib/App/Bondage/Common.pm
+++ lib/App/Bondage/Common.pm
@@ -7,7 +7,7 @@ require Exporter;
 our @ISA = qw( Exporter );
 our @EXPORT = qw( $VERSION $HOMEPAGE $CRYPT_SALT );
 
-our $VERSION    = '0.2.2';
+our $VERSION    = '0.2.3';
 our $HOMEPAGE   = 'http://search.cpan.org/dist/App-Bondage';
 our $CRYPT_SALT = 'erxpnUyerCerugbaNgfhW';
 

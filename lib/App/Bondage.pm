9bf6bc6a2c5b23f961d7d2353f96aad7285dc346
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2008-09-24 20:10:55 +0000

    0.3.2 stuff

--- lib/App/Bondage.pm
+++ lib/App/Bondage.pm
@@ -23,7 +23,7 @@ use POE::Component::IRC::Plugin::NickReclaim;
 use POE::Component::IRC::Plugin::NickServID;
 use Socket qw(inet_ntoa);
 
-our $VERSION    = '0.3.1';
+our $VERSION    = '0.3.2';
 our $HOMEPAGE   = 'http://search.cpan.org/perldoc?App::Bondage';
 our $CRYPT_SALT = 'erxpnUyerCerugbaNgfhW';
 
